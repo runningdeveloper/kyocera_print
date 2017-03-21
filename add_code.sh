3dbfaba1d69d2d66b998089ab681f0af7239d957
Geoffrey <info@runningdeveloper.com>
2017-03-21 13:39:55 +0200

    renamed printer kyocera and had an error with single quotes not showing variable

--- add_code.sh
+++ add_code.sh
@@ -2,6 +2,6 @@
 
 MY_CODE="$1"
 
-sudo sed -i '.ppd' 's/KmManagment MG00000002\/00000002\: "(00000002)/KmManagment MG$MY_CODE\/$MY_CODE\: "($MY_CODE)/g' /etc/cups/ppd/kyo_1.ppd
+sudo sed -i ".ppd" "s/KmManagment MG00000000\/00000000\: \"(00000000)/KmManagment MG$MY_CODE\/$MY_CODE\: \"($MY_CODE)/g" /etc/cups/ppd/kyocera.ppd
 
 exit 0
